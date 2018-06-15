e341c85814c880f6bfe9885502aaf510fead2dbc
Edsel Caprice <caprice@mail.usf.edu>
2018-06-15 01:25:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,7 +63,7 @@ end
 def won?(board)
     WIN_COMBINATIONS.detect do |combination|
   
-      (board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X") ||
+     if (board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X") ||
          (board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O")
         
         return combination
